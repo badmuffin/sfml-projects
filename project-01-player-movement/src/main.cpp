332506fa6daa1c46f555269101ce04293bffa773
badmuffin <itsabhijeet06@gmail.com>
2026-05-14 23:43:07 +0530

    feat: basic setup for project 1

---
+++ project-01-player-movement/src/main.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1080, 800), "Top down player", sf::Style::Resize);
+    
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        
+    }
+}
